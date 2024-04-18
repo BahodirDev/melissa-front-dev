cce8c456ddc489001505990cdd032bd5310326a1
vosid <temirboyev0823@gmail.com>
2024-04-18 09:47:06 +0500

    goods page pagination on search fixed

--- src/pages/goods/Goods.jsx
+++ src/pages/goods/Goods.jsx
@@ -240,7 +240,7 @@ export default function Goods() {
 			if (data.status === 200) {
 				setTotalPage(Math.ceil(data?.data[0]?.full_count / limit))
 				setFilteredData(data?.data)
-				if (!data?.data?.data?.length) setCurrentPage(1)
+				if (!data?.data?.length) setCurrentPage(1)
 			} else {
 				setTotalPage(1)
 				toast.error("Nomalum server xatolik")
