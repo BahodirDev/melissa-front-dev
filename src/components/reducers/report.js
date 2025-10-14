b343a5f5a8545306201f14d082138814bde2f872
BahodirDev <mydatabases714@gmail.com>
2025-10-14 18:40:24 +0500

    FIx: Hisobot details hanlded by another api

--- src/components/reducers/report.js
+++ src/components/reducers/report.js
@@ -1,93 +1,98 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 export const reportSlice = createSlice({
-	name: "report",
-	initialState: {
-		data: [],
-		loading: false,
-		capital: 0,
-		benefit: 0,
-		income: 0,
-		outcome: 0,
-		qtyIn: 0,
-		qtyOut: 0,
-	},
-	reducers: {
-		setData: (state, action) => {
-			state.data = action.payload
-		},
-		setLoading: (state, action) => {
-			state.loading = action.payload
-		},
-		addData: (state, action) => {
-			state.data = [...state.data, action.payload]
-		},
-		editData: (state, action) => {
-			const index = state.data.findIndex(
-				(item) => item.store_id === action.payload.store_id
-			)
-			if (index !== -1) {
-				state.data[index] = {
-					...state.data[index],
+  name: "report",
+  initialState: {
+    data: [],
+    loading: false,
+    detailsLoading: false,
+    capital: 0,
+    benefit: 0,
+    income: 0,
+    outcome: 0,
+    qtyIn: 0,
+    qtyOut: 0,
+  },
+  reducers: {
+    setData: (state, action) => {
+      state.data = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setDetailsLoading: (state, action) => {
+      state.detailsLoading = action.payload;
+    },
+    addData: (state, action) => {
+      state.data = [...state.data, action.payload];
+    },
+    editData: (state, action) => {
+      const index = state.data.findIndex(
+        (item) => item.store_id === action.payload.store_id
+      );
+      if (index !== -1) {
+        state.data[index] = {
+          ...state.data[index],
 
-					store_name: action.payload?.store_name,
-				}
-			}
-		},
-		removeData: (state, action) => {
-			const index = state.data.findIndex(
-				(item) => item.reports_id === action.payload
-			)
+          store_name: action.payload?.store_name,
+        };
+      }
+    },
+    removeData: (state, action) => {
+      const index = state.data.findIndex(
+        (item) => item.reports_id === action.payload
+      );
 
-			if (index !== -1) {
-				state.data.splice(index, 1)
-			}
-		},
-		setCapital: (state, action) => {
-			state.capital = action.payload
-		},
-		setQtyIn: (state, action) => {
-			state.qtyIn = action.payload
-		},
-		setQtyOut: (state, action) => {
-			state.qtyOut = action.payload
-		},
-		setBenefit: (state, action) => {
-			state.benefit = action.payload
-		},
-		setIncome: (state, action) => {
-			state.income = action.payload
-		},
-		setOutcome: (state, action) => {
-			state.outcome = action.payload
-		},
-		editDate: (state, action) => {
-			const index = state.data.findIndex(
-				(item) => item.reports_id === action.payload
-			)
+      if (index !== -1) {
+        state.data.splice(index, 1);
+      }
+    },
+    setCapital: (state, action) => {
+      state.capital = action.payload;
+    },
+    setQtyIn: (state, action) => {
+      state.qtyIn = action.payload;
+    },
+    setQtyOut: (state, action) => {
+      state.qtyOut = action.payload;
+    },
+    setBenefit: (state, action) => {
+      state.benefit = action.payload;
+    },
+    setIncome: (state, action) => {
+      state.income = action.payload;
+    },
+    setOutcome: (state, action) => {
+      state.outcome = action.payload;
+    },
+    editDate: (state, action) => {
+      const index = state.data.findIndex(
+        (item) => item.reports_id === action.payload
+      );
 
-			if (index !== -1) {
-				state.data[index] = {
-					...state.data[index],
-					reports_createdat: action.payload?.reports_createdat,
-				}
-			}
-		},
-	},
-})
+      if (index !== -1) {
+        state.data[index] = {
+          ...state.data[index],
+          reports_createdat: action.payload?.reports_createdat,
+        };
+      }
+    },
+  },
+});
 
 export const {
-	setData,
-	setLoading,
-	addData,
-	editData,
-	removeData,
-	setCapital,
-	setBenefit,
-	setIncome,
-	setOutcome,
-	editDate,
-	setQtyIn,
-	setQtyOut,
-} = reportSlice.actions
-export default reportSlice.reducer
+  setData,
+  setLoading,
+  setDetailsLoading,
+  addData,
+  editData,
+  removeData,
+  setCapital,
+  setBenefit,
+  setIncome,
+  setOutcome,
+  editDate,
+  setQtyIn,
+  setQtyOut,
+} = reportSlice.actions;
+export default reportSlice.reducer;
